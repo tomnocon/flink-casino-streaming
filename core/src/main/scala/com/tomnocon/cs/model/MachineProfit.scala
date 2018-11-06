6d06e98dfc05fc8cac93880e759887d1bd2db193
Tomasz Nocoń <thom.nocon@gmail.com>
2018-11-06 16:48:56 +0100

    Update dashboards to use cumulative sum

--- core/src/main/scala/com/tomnocon/cs/model/MachineProfit.scala
+++ core/src/main/scala/com/tomnocon/cs/model/MachineProfit.scala
@@ -1,5 +1,6 @@
 package com.tomnocon.cs.model
 
+// machine income
 case class MachineProfit(machineId: String, value: Long, gameId: String, siteId: String, timestamp: Long) {
 
   override def toString: String = s"MachineProfit{machineId=$machineId, value=$value, gameId=$gameId, siteId=$siteId, timestamp=$timestamp}"
