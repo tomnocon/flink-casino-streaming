db9a5e0d7b6fb1e5bcdffe7833e66df31a711d32
Tomasz Nocoń <thom.nocon@gmail.com>
2018-12-18 15:23:36 +0100

    Fix fat jar configuration

--- env/elasticdump/dump.sh
+++ env/elasticdump/dump.sh
@@ -1,3 +1,5 @@
-elasticdump --input=http://elasticsearch:9200/.kibana_1 --output=$ --retryAttempts 10 --retryDelay 2000
-elasticdump --input=/var/elasticdump/kibana_mapping.json --output=http://elasticsearch:9200/.kibana_1 --type=mapping
-elasticdump --input=/var/elasticdump/kibana_data.json --output=http://elasticsearch:9200/.kibana_1 --type=data
+until elasticdump --input=http://elasticsearch:9200/.kibana_1 --output=$;
+do
+    elasticdump --input=/var/elasticdump/kibana_mapping.json --output=http://elasticsearch:9200/.kibana_1 --type=mapping
+    elasticdump --input=/var/elasticdump/kibana_data.json --output=http://elasticsearch:9200/.kibana_1 --type=data
+done
